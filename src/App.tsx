5325f3778704240043eb17e8f5fa07b7b4916d80
Alex Ngassaki <alexngassaki@icloud.com>
2026-03-26 14:30:52 +0100

    Ajout de vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Acceuil from "./pages/Acceuil/acceuil";
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   
   return (
     <>
       <Acceuil/>
+      <Analytics/>
     </>
   )
 }
